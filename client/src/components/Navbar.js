3363be154eeb8da9a1b299c9e1e9954e6315c817
ericbanker12 <ericbanker12@gmail.com>
2019-10-04 12:13:30 -0500

    create useLocalStorage, rewrite Navbar, add darkmode

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,30 @@
 import React from 'react'
 
+import useLocalStorage from '../hooks/useLocalStorage'
+
 function Navbar() {
+    const [darkMode, setDarkMode] = useLocalStorage(false)
+
+    function darkModeHandler() {
+        setDarkMode(!darkMode)
+    }
+
+    React.useEffect(()=>{
+        const theme = document.createElement('link')
+        theme.href=`https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/${darkMode?'darkly':'flatly'}/bootstrap.min.css`
+        theme.rel='stylesheet'
+        document.head.appendChild(theme)
+        return () => {document.head.removeChild(theme)}
+    },[darkMode])
+
     return (
-        <div></div>
+        <div>
+            <button
+                onClick={darkModeHandler}
+            >
+                {darkMode?'Dark Mode':'Light Mode'}
+            </button>
+        </div>
     )
 }
 

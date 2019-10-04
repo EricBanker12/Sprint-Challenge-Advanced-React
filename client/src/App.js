8a654cbd2c0dfdd8bf80011b777701908442be39
ericbanker12 <ericbanker12@gmail.com>
2019-10-04 10:36:29 -0500

    add Axios dependency, convert App to class, get player data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import axios from 'axios'
+
 import './App.css'
 
-function App() {
-    return (
-        <div className="App">
-            
-        </div>
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            players: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:5000/api/players`)
+            .then(resp => {
+                // console.log(resp)
+                this.setState({players: resp.data})
+            })
+            .catch(console.error)
+    }
+
+    render() {
+        return (
+            <div className='App'>
+
+            </div>
+        )
+    }
 }
 
 export default App
